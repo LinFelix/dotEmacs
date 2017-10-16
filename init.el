7fc42bec27ea7708b7b566bdfaabef95f8768ada
LinFelix <felix-voelker@gmx.net>
2017-10-16 14:08:21 +0200

    add all the existing stuff

---
+++ init.el
@@ -0,0 +1,41 @@
+;;; Commentary:
+
+;;; Code:
+(package-initialize)
+
+(add-to-list 'load-path "~/progBin/org-mode/lisp")
+(add-to-list 'load-path "~/progBin/org-mode/contrib/lisp" t)
+;;  we want a couple of sane defaults
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/sanity.org")
+
+;; we want a nice layout
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/layout.org")
+
+;; langtools
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/langsupport.org")
+
+;; langs
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/langs.org")
+
+;; completion
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/completions.org")
+
+;; otter tools
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/otters.org")
+
+;; org
+(org-babel-load-file "~/.emacs.d/peoplesEmacs/org.org")
+
+;; set font
+(add-to-list 'default-frame-alist
+             '(font . "Fira Mono-11"))
+
+;; get a theme
+(use-package dracula-theme
+  :ensure t
+  :config (load-theme 'dracula t)
+  (set-background-color "black"))
+
+(provide 'init)
+;;; init.el ends here
+
