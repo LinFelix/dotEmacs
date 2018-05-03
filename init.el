978fc5abda137100c8bd690a762eef029c02c568
LinFelix <felix-voelker@gmx.net>
2018-05-03 17:40:12 +0200

    some sanity

--- init.el
+++ init.el
@@ -15,14 +15,62 @@
 
 ;;; Code:
 
+
+(defun peoplesEmacs/no-prompts  ()
+  (fset 'yes-or-no-p 'y-or-n-p)
+  (setq vc-follow-symlinks t))
+
+(defun peoplesEmacs/no-fluff ()
+  (tool-bar-mode 0)
+  (scroll-bar-mode 0)
+  (menu-bar-mode 0)
+  (setq inital-scratch-message nil)
+  (peoplesEmacs/no-prompts))
+
+(defun peoplesEmacs/use-utf-8 ()
+  (set-default-coding-systems 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (setq buffer-file-coding-system 'utf-8))
+
+(defun peoplesEmacs/setup-packages ()
+  (package-initialize)
+  (require 'package)
+  (setq package-archives
+        (quote
+         (("gnu" . "https://elpa.gnu.org/packages/")
+          ("melpa" . "https://melpa.org/packages/")
+          ("marmalade" . "https://marmalade-repo.org/packages/"))))
+  (package-refresh-contents)
+  (when (not (require 'use-package nil 'noerror))
+           (package-refresh-contents)
+           (package-install 'use-package))
+  (auto-package-update-now))
+
+(defun peoplesEmacs/use-neo-layout ()
+  (global-set-key (kbd "C-ö") ctl-x-map)
+  (use-package evil
+    :ensure t
+    :config
+    ))
+    
+;;; Config:
+
 ;; I'm still unsure why this line is necessary, but for reference, here:
 ;; [[https://emacs.stackexchange.com/questions/5828/why-do-i-have-to-add-each-package-to-load-path-or-problem-with-require-packag]]
 ;; [[https://github.com/jwiegley/use-package/issues/275]]
 (package-initialize)
 
-;; (add-to-list 'load-path "~/progBin/org-mode/lisp")
-;; (add-to-list 'load-path "~/progBin/org-mode/contrib/lisp")
-;; ;;  we want a couple of sane defaults
+;; TODO: put a new version of org-mode in a script with a reasonable
+;; PATH
+(add-to-list 'load-path "~/progBin/org-mode/lisp")
+(add-to-list 'load-path "~/progBin/org-mode/contrib/lisp")
+
+(peoplesEmacs/no-fluff)
+(peoplesEmacs/use-utf-8)
+(peoplesEmacs/setup-packages)
+;; we want a couple of sane defaults
 ;; (org-babel-load-file "~/.emacs.d/peoplesEmacs/sanity.org")
 
 ;; ;; we want a nice layout
@@ -60,3 +108,4 @@
 
 ;; (provide 'init)
 ;; ;;; init.el ends here
+
